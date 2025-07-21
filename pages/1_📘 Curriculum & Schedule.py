379fcec6523ff84471934fad8a6a0a51125f0f73
davidsamanyaporn <d.samanyaporn@icloud.com>
2025-07-21 15:59:55 +0700

    requirements.txt

--- pages/1_📘 Curriculum & Schedule.py
+++ pages/1_📘 Curriculum & Schedule.py
@@ -2,15 +2,16 @@ import streamlit as st
 
 st.title("📘 Course Curriculum")
 st.markdown("""
-### Week 1: Python for Data Science
-- Variables, Lists, Dictionaries
-- Pandas, NumPy
+### Week 1: Siam East Item Code Ai Search DEMO:
+- Embeddinfg, Vector Databases
+- Postgres Database with pgvector
+- Similarity Search
 
-### Week 2: AI Foundations
-- Linear Regression, Logistic Regression
-- Loss functions, Gradient Descent
+### Week 2: LLM DEMO for Siam East Item Code Ai Search:
+- Ollama, OpenAi API
+- LLM Integration with Result return from Similarity Search
 
-### Week 3: Deep Learning
+### Week 3: LLM & Langchain Class#1
 - Neural Networks with TensorFlow/Keras
 - CNN, RNN, LSTM, Transformers
 
